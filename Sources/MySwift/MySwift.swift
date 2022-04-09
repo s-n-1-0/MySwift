49edf0420ccb8907ac49f5b4468174f326dd1eee
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-10 00:13:18 +0900

    swift package init

---
+++ Sources/MySwift/MySwift.swift
@@ -0,0 +1,6 @@
+public struct MySwift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MySwiftTests/MySwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MySwift
+
+final class MySwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MySwift().text, "Hello, World!")
+    }
+}
