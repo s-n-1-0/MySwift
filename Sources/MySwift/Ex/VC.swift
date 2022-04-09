65ad23e9f0e770ebdbf109bdd0715b70500cbe19
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-04-10 00:52:52 +0900

    現在表示しているViewControllerを取得するgetPresentedViewControllerの追加

---
+++ Sources/MySwift/Ex/VC.swift
@@ -0,0 +1,29 @@
+//
+//  VC.swift
+//  
+//
+//  Created by Mac10 on 2022/04/10.
+//
+
+import SwiftUI
+import UIKit
+extension View{
+    public func getPresentedViewController()->UIViewController?{
+        UIViewController.getPresentedViewController()
+    }
+}
+extension UIViewController{
+    public func getPresentedViewController()->UIViewController?{
+        UIViewController.getPresentedViewController()
+    }
+    public static func getPresentedViewController()->UIViewController?{
+         guard var visibleViewController = UIApplication.shared.windows.first?.rootViewController else {
+             return nil
+         }
+         while let vc = visibleViewController.presentedViewController {
+             visibleViewController = vc
+         }
+         return visibleViewController
+     }
+    
+}
