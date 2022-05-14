d2c515c633577482e6fcdee45a5d0b4727846f4c
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-05-14 21:17:23 +0900

    Dateの時間以下を切り落とすtruncateHms関数を追加

--- Sources/MySwift/Ex/Date.swift
+++ Sources/MySwift/Ex/Date.swift
@@ -7,5 +7,12 @@
 
 import Foundation
 extension Date{
-
+    /**
+      時間以下を切り落とします。
+     */
+    func truncateHms() -> Date{
+        let cal = Calendar.gregorianCalendar
+        let comps = cal.dateComponents([.year, .month, .day], from: self)
+        return cal.date(from: comps)!
+    }
 }
