f3d7213cd2fcd646dedcd5daa829a608331cfe48
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-08-20 22:58:20 +0900

    SKProductの通貨記号付き価格を取得するlocalizedPriceを追加

---
+++ Sources/MySwift/Ex/SKProduct.swift
@@ -0,0 +1,22 @@
+//
+//  SKProduct.swift
+//  
+//
+//  Created by Mac10 on 2022/08/20.
+//
+
+import  StoreKit
+extension SKProduct{
+    /**
+       通貨記号付き価格
+       https://developer.apple.com/documentation/storekit/skproduct/1506094-price
+     */
+   public var localizedPrice:String {
+        get{
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .currency
+            numberFormatter.locale = p.priceLocale
+            return numberFormatter.string(from: p.price)
+        }
+    }
+}
