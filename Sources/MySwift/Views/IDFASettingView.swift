d3b5e8f0ade1dfb5a500f5cedf90b4919e8a042f
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2025-05-11 05:31:24 +0900

    日記アプリからIDFA設定Viewを持ってくる(仮)

---
+++ Sources/MySwift/Views/IDFASettingView.swift
@@ -0,0 +1,66 @@
+//
+//  IDFASettingView.swift
+//
+//  Created by Mac10 on 2020/12/30.
+//  Copyright © 2020 Mac10. All rights reserved.
+//
+
+import AppTrackingTransparency
+import SwiftUI
+
+public struct IDFASettingView: View {
+    public let appColor:Color
+    public let textColor:Color
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    public var body: some View {
+        VStack {
+            Text("").frame(height: 15)
+            HStack {
+                Spacer()
+                Image(systemName: "person.fill").resizable().frame(width: 25, height: 25).foregroundColor(appColor)
+                Text("ST-IDFATitle").font(.title)
+                Spacer()
+            }
+            Divider()
+            HStack {
+                Spacer()
+                VStack {
+                    Text("ST-IDFAAnnounce").font(.body)
+                    Text("ST-IDFAKome").font(.caption)
+                }
+                Spacer()
+            }
+            Divider()
+            Spacer(minLength: 15)
+            Button(action: {
+                switch ATTrackingManager.trackingAuthorizationStatus {
+                case .authorized, .denied, .restricted:
+                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                case .notDetermined:
+                    ATTrackingManager.requestTrackingAuthorization(completionHandler: { _ in })
+                default:
+                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                }
+                dismiss()
+            }) {
+                HStack {
+                    Text("ST-Continue")
+                }.frame(maxWidth: .infinity).padding([.top, .bottom]).foregroundColor(textColor)
+            }.accentColor(textColor).background(appColor)
+            .frame(width: 150).cornerRadius(10)
+            Spacer(minLength: 15)
+        }
+    }
+}
+
+struct IDFASettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        IDFASettingView(appColor: .red, textColor: .white)
+    }
+}
