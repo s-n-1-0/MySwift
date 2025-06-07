c2de41c290a63ed125b247869eeaf3aeca2fe1c0
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2025-06-08 02:18:31 +0900

    アプリ名をアナウンス文に埋め込む

--- Sources/MySwift/Views/iCloud/CloudStatusSection.swift
+++ Sources/MySwift/Views/iCloud/CloudStatusSection.swift
@@ -10,13 +10,17 @@ import SwiftUI
 
 public struct CloudStatusSection: View {
     public init() {
+        let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
+        
+        annouceText = "CloudAnnounce".localizedModule.convertAt(with: appName ?? "")
         iCloudMode = (FileManager.default.url(forUbiquityContainerIdentifier: nil) != nil) ? "CloudAllow" : "CloudDeny"
     }
     
     let iCloudMode: String
+    let annouceText:String
     
     public var body: some View {
-        Section(header: Text("iCloud"), footer: Text("CloudAnnounce".localizedModule)) {
+        Section(header: Text("iCloud"), footer: Text(annouceText)) {
             HStack {
                 Image(systemName: "cloud.fill")
                 Text("CloudAccess".localizedModule)
