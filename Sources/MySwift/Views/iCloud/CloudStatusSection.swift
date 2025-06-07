da50ddbbdba9654d48dc3e8789bfddaed42f15b9
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2025-06-08 02:33:13 +0900

    CloudKitに対応する

--- Sources/MySwift/Views/iCloud/CloudStatusSection.swift
+++ Sources/MySwift/Views/iCloud/CloudStatusSection.swift
@@ -6,17 +6,18 @@
 //
 
 import SwiftUI
-
+import CloudKit
 
 public struct CloudStatusSection: View {
-    public init() {
+    public init(cloudType:CloudType) {
         let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
         
         annouceText = "CloudAnnounce".localizedModule.convertAt(with: appName ?? "")
-        iCloudMode = (FileManager.default.url(forUbiquityContainerIdentifier: nil) != nil) ? "CloudAllow" : "CloudDeny"
+        self.cloudType = cloudType
     }
     
-    let iCloudMode: String
+    @State private var iCloudMode: String = ""
+    let cloudType:CloudType
     let annouceText:String
     
     public var body: some View {
@@ -27,12 +28,36 @@ public struct CloudStatusSection: View {
                 Spacer()
                 Text(iCloudMode.localizedModule)
             }
+        }.onAppear{
+            switch cloudType{
+            case .Document:
+                iCloudMode = (FileManager.default.url(forUbiquityContainerIdentifier: nil) != nil) ? "CloudAllow" : "CloudDeny"
+            case .Kit:
+                let container = CKContainer.default()
+
+                 container.accountStatus { status, error in
+                     Task{
+                         @MainActor in
+                         switch status {
+                         case .available:
+                             iCloudMode = "CloudAllow"
+                         default:
+                             iCloudMode = "CloudDeny"
+                         }
+                     }
+                 }
+            }
         }
     }
 }
 
+public enum CloudType{
+    case Kit
+    case Document
+}
+
 #Preview {
     List{
-        CloudStatusSection()
+        CloudStatusSection(cloudType: .Kit)
     }
 }
